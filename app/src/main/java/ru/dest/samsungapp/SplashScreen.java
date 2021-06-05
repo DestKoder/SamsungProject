9c1f794adb1e7ebd9f6e74a9e215fb20fa545754
reszi123 <imrezz07@gmail.com>
2021-06-05 16:22:38 +0300

    +дизайн

---
+++ app/src/main/java/ru/dest/samsungapp/SplashScreen.java
@@ -0,0 +1,50 @@
+package ru.dest.samsungapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    private Handler handler=new Handler();
+    private  boolean isLauncher=false;
+    private Timer timer=new Timer();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startLauncherActivity();
+            }
+        },2500);
+    }
+
+    private void startLauncherActivity() {
+        if(!isLauncher){
+            isLauncher=true;
+            Intent intent=new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(intent);
+        }
+        finish();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(handler!=null){
+            handler.removeCallbacks(null);
+        }
+        if(timer!=null){
+            timer.cancel();
+        }
+
+    }
+}
