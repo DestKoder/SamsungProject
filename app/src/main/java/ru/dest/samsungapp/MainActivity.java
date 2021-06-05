242ec6e11b1d6dfa396abf13dea49657c5556cf4
reszi123 <imrezz07@gmail.com>
2021-06-05 15:48:58 +0300

    +дизайн

--- app/src/main/java/ru/dest/samsungapp/MainActivity.java
+++ app/src/main/java/ru/dest/samsungapp/MainActivity.java
@@ -1,26 +1,14 @@
 package ru.dest.samsungapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
-
-import ru.dest.samsungapp.api.Api;
 import ru.dest.samsungapp.api.BinanceApi;
-import ru.dest.samsungapp.api.JsonGetTask;
 
 public class MainActivity extends AppCompatActivity {
-
 
     private static HashMap<String,String> data;
 
@@ -29,34 +17,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView price = (TextView)findViewById(R.id.text_btc);
-
-//        String res = null;
-//
-//        try {
-//            res = new JsonGetTask().execute("https://api.binance.com/api/v3/ticker/price", "symbol", "BTCUSDT").get();
-//        } catch (ExecutionException e) {
-//            e.printStackTrace();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//        Gson gson = new Gson();
-//
-//        Type type = new TypeToken<Map<String,String>>(){}.getType();
-//
-//        Map<String,String> answer = gson.fromJson(res,type);
-//
-//        price.setText(answer.get("price"));
+        TextView price_btc = (TextView)findViewById(R.id.bitcoin);
+        TextView price_eth = (TextView)findViewById(R.id.ethereum);
 
         HashMap<String,String> binanceData  = new BinanceApi("https://api.binance.com/api/v3/ticker/price").getData();
 
         if(!binanceData.isEmpty()){
-            String text = "BTC: " + binanceData.get("bitcoin") + " ETH: " + binanceData.get("etherium");
-
-            price.setText(text);
+            String text_btc = "BTC: " + binanceData.get("bitcoin");
+            price_btc.setText(text_btc);
+            String text_eth = "ETH: " + binanceData.get("etherium");
+            price_eth.setText(text_eth);
         }
 
+        Spinner spinner = findViewById(R.id.spinner_api);
+        String[] api = {"Binance", "Other"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, api);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
-
 }
